0b04f77e8fd5801ab1d10e697ca0393939253877
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2025-06-25 12:31:34 +0530

    Product variant fragment changes according to boxup needs

--- src/apollo/types.ts
+++ src/apollo/types.ts
@@ -21729,7 +21729,7 @@ export type ProductVariantFragment = (
       & { value: AttributeValue['name'] }
     )>> }>, product: (
     Pick<Product, 'id' | 'name' | 'slug' | 'isAvailableForPurchase' | 'collections'>
-    & { weight: Maybe<Pick<Weight, 'unit' | 'value'>>, category: Maybe<Pick<Category, 'id' | 'name' | 'slug'>>, thumbnail: Maybe<Pick<Image, 'url' | 'alt'>>, productType: Pick<ProductType, 'id' | 'isShippingRequired'>, metadata: Array<Maybe<Pick<MetadataItem, 'key' | 'value'>>>, tags: Maybe<Array<Maybe<Pick<TagType, 'name'>>>> }
+    & { collection: Maybe<Pick<Collection, 'id' | 'name' | 'slug'> & {metadata: Array<Maybe<Pick<MetadataItem, 'key' | 'value'>>>}>, weight: Maybe<Pick<Weight, 'unit' | 'value'>>, category: Maybe<Pick<Category, 'id' | 'name' | 'slug'>>, thumbnail: Maybe<Pick<Image, 'url' | 'alt'>>, productType: Pick<ProductType, 'id' | 'isShippingRequired'>, metadata: Array<Maybe<Pick<MetadataItem, 'key' | 'value'>>>, tags: Maybe<Array<Maybe<Pick<TagType, 'name'>>>> }
   ) }
 );
 
@@ -22647,6 +22647,14 @@ export const ProductVariantFragmentDoc = gql`
       id
       isShippingRequired
     }
+    collections {
+            id
+            name
+            metadata {
+              key
+              value
+            }
+          }
     metadata {
       key
       value
